6e4a7db012915c43ff6235f0ca3c301c9bf46d13
valery76 <walerka100@yandex.ru>
2020-03-07 09:28:24 +0300

    added sendgrid add-on for heroku mailing

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -12,7 +12,8 @@ class SubscriptionsController < ApplicationController
     @new_subscription.user = current_user
 
     if @new_subscription.save
-      # Если сохранилась успешно, редирект на страницу самого события
+      EventMailer.subscription(@event, @new_subscription).deliver_now
+
       redirect_to @event, notice: I18n.t('controllers.subscriptions.created')
     else
       # если ошибки — рендерим здесь же шаблон события

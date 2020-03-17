96f6817893258be928b86870c2f9242997407bc2
Valery <walerka100@yandex.ru>
2020-03-17 15:54:46 +0300

    added pincode to event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,6 +8,9 @@ class EventsController < ApplicationController
   # Задаем объект @event от текущего юзера для других действий
   before_action :set_current_user_event, only: [:edit, :update, :destroy]
 
+  # Проверка пинкода перед отображением события
+  before_action :password_guard!, only: [:show]
+
   def index
     @events = Event.all
   end
@@ -50,6 +53,22 @@ class EventsController < ApplicationController
 
   private
 
+  def password_guard!
+    return true if @event.pincode.blank?
+    return true if user_signed_in? && current_user == @event.user
+    if params[:pincode].present? && @event.pincode_valid?(params[:pincode])
+      cookies.permanent["events_#{@event.id}_pincode"] = params[:pincode]
+    end
+
+    pincode = cookies.permanent["events_#{@event.id}_pincode"]
+    unless @event.pincode_valid?(pincode)
+      if params[:pincode].present?
+        flash.now[:alert] = I18n.t('controllers.events.wrong_pincode')
+      end
+      render 'password_form'
+    end
+  end
+
   def set_current_user_event
     @event = current_user.events.find(params[:id])
   end
@@ -59,6 +78,7 @@ class EventsController < ApplicationController
   end
 
   def event_params
-    params.require(:event).permit(:title, :address, :datetime, :description)
+    params.require(:event).
+      permit(:title, :address, :datetime, :description, :pincode)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_121103) do
+ActiveRecord::Schema.define(version: 2020_03_16_142957) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2020_03_02_121103) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "pincode"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
